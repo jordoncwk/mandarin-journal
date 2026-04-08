5e8cac44e3c152aac574985853366a46d3a753db
jordoncwk <jordoncwk@example.com>
2026-04-08 20:12:21 +0800

    feat: register English journal routes in app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ import { renderJournal } from './journal.js';
 import { renderForm } from './form.js';
 import { renderDetail } from './detail.js';
 import { renderFlashcard } from './flashcard.js';
+import { renderEnglish } from './english.js';
+import { renderEnglishForm } from './english-form.js';
+import { renderEnglishDetail } from './english-detail.js';
 import { initSync } from './sync.js';
 
 if ('serviceWorker' in navigator) {
@@ -14,6 +17,10 @@ register('#add', renderForm);
 register('#edit', renderForm);
 register('#detail', renderDetail);
 register('#flashcard', renderFlashcard);
+register('#english', renderEnglish);
+register('#english-add', renderEnglishForm);
+register('#english-edit', renderEnglishForm);
+register('#english-detail', renderEnglishDetail);
 
 initSync();
 startRouter();
